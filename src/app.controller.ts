d492ebe2168a3584c74beebbb73a86f8719d835e
ralcayag <ralcayaga74@gmail.com>
2024-07-11 20:57:28 -0400

    endpoint artistas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Artista } from './artistas';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  artistas: Artista []= [];
+
+
+  constructor(private readonly appService: AppService) {
+    this.artistas.push(new Artista(1,'Rodrigo', 'bio', 'banda', 'chilena',['rock'], 1));
+  }
+
+
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('artistas')
+  obtenerArtistas(): Artista[] {
+    //this.artistas.push(new Artista(1,'Rodrigo', 'bio', 'banda', 'chilena',['rock'], 1));
+    return this.artistas;
+  }
+
+  @Post('artistas')
+  nuevoArtista(@Body() artista:Artista):Artista{
+    console.log(artista);
+    this.artistas.push(artista);
+    return null;
+  }
+
 }
---
+++ src/artistas.ts
@@ -0,0 +1,14 @@
+export class Artista{
+
+     constructor(
+            
+            public  id : number,
+            public  nombre : string,
+            public  biografia  : string,
+            public  tipo  : string,
+            public  pais : string,
+            public  generoMusical : string[],
+            public  oyentesMensuales : number
+     ){}
+
+}
